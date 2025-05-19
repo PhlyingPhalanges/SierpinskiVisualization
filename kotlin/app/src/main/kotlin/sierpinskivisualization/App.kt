e746dde8132aade64728aacb66ba95c5f283f3f0
Max Statz <maxstatz@Maxs-MBP.lan>
2025-05-19 17:06:52 -0500

    Changes to project structure

---
+++ kotlin/app/src/main/kotlin/sierpinskivisualization/App.kt
@@ -0,0 +1,6 @@
+package sierpinskivisualization
+
+fun main() {
+    println("Please select the fractal object you would like to see generated:\n" +
+        "[1] Sierpiński Carpet\n[2] Sierpiński Gasket\n")
+}
---
+++ kotlin/app/src/test/kotlin/sierpinskivisualization/AppTest.kt
@@ -0,0 +1,5 @@
+package sierpinskivisualization
+
+class AppTest {
+
+}
