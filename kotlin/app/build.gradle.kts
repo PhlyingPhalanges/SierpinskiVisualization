e0d2161c720e2b16b8757fe45617265e147cec9b
Max Statz <maxstatz@Maxs-MBP.lan>
2025-05-19 17:27:23 -0500

    Properly reading in user input for choice of fractal object and number of iterations

--- kotlin/app/build.gradle.kts
+++ kotlin/app/build.gradle.kts
@@ -47,3 +47,7 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.withType<JavaExec> {
+    standardInput = System.`in`
+}
--- kotlin/app/src/main/kotlin/sierpinskivisualization/App.kt
+++ kotlin/app/src/main/kotlin/sierpinskivisualization/App.kt
@@ -3,4 +3,19 @@ package sierpinskivisualization
 fun main() {
     println("Please select the fractal object you would like to see generated:\n" +
         "[1] Sierpiński Carpet\n[2] Sierpiński Gasket\n")
+    val object_choice: String? = readLine()
+    var choice_show: String = ""
+    if (object_choice == "1") {
+        choice_show = "Sierpiński Carpet"
+    } else if (object_choice == "2") {
+        choice_show = "Sierpiński Gasket"
+    }
+    println("How many iterations of the $choice_show would you like generated?\n")
+    val iterations_choice: String? = readLine()
+    val num_iterations = iterations_choice?.toIntOrNull()
+    if (object_choice == "1") {
+        // call carpet method, passing in num_iterations
+    } else if (object_choice == "2") {
+        // call gasket method, passing in num_iterations
+    }
 }
